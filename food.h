46d4ca023c47c56fd33f0617244f35e6785cd21c
Rafaela Petriuc <rafaelapetriuc@yahoo.com>
2019-12-12 11:01:06 +0200

    initial

--- food.h
+++ food.h
@@ -1,12 +1,12 @@
 //
 // Created by Lenovo on 11/6/2019.
 //
-#define MAX_FOOD_NAME 17
+#define MAX_FOOD_NAME 50
 #ifndef FOOD_ORDERING_FOOD_H
 #define FOOD_ORDERING_FOOD_H
 
-void displayFoodOptions(int noOfFoods, char food[][MAX_FOOD_NAME]);
-void displayFoodType(int noTypes, char food[], char types[][MAX_FOOD_NAME], double prices[]);
-void displayDrinks(char food[][MAX_FOOD_NAME], int foodChoice, int nodrinks, char drinks[][30], double drinksPrices[6]);
+void displayFoodOptions(int noOfFoods, char **foods);
+void displayFoodType(int noOfTypes, char food[], char **types, double *prices);
+void displayDrinks(char **foods, int foodChoice, int noOfDrinks, char **drinks, double *drinksPrices);
 
 #endif //FOOD_ORDERING_FOOD_H
